2368563d9e3def220d009c8701ed4b29809c22c5
tonyley <tonyley3@gmail.com>
2015-07-03 13:05:32 +0800

    Add Cordova

---
+++ app/src/main/java/com/wisape/android/cordova/PhotoSelectorPlugin.java
@@ -0,0 +1,17 @@
+package com.wisape.android.cordova;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONException;
+
+/**
+ * Created by LeiGuoting on 3/7/15.
+ */
+public class PhotoSelectorPlugin extends CordovaPlugin {
+
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+
+        return false;
+    }
+}
