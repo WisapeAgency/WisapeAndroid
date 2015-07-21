01bb29f160a1f48a10f1b77ed2d29c0fc0e1491d
tonylei <tonyley3@gmail.com>
2015-07-21 11:30:55 +0800

    Add test for StoryLogic

---
+++ app/src/androidTest/java/com/wisape/android/logic/StoryLogicTest.java
@@ -0,0 +1,18 @@
+package com.wisape.android.logic;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by tony on 2015/7/21.
+ */
+public class StoryLogicTest extends AndroidTestCase{
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testListStoryTemplate(){
+
+    }
+}
