a0411fe5a9cb7438dea60c7cddb080f8542a8cb4
liushiping <liushiping@incito.com.cn>
2015-08-11 16:25:05 +0800

    remove code

--- app/src/main/java/com/wisape/android/activity/StoryTemplateActivity.java
+++ app/src/main/java/com/wisape/android/activity/StoryTemplateActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.webkit.JavascriptInterface;
 
 import com.wisape.android.util.EnvironmentUtils;
 
@@ -32,63 +31,6 @@ public class StoryTemplateActivity extends AbsCordovaActivity {
 
     public void invodeJavascriptTest(){
         appView.loadUrl("javascript:test2()");
-    }
-
-    /**
-     * JS调用接口，用于保存Story
-     * @param storyId 故事id
-     * @param html story的html代码
-     * @param paths 文件真实路径和相对路径(数组)
-     */
-    @JavascriptInterface
-    public boolean save(int storyId, String html, String[] paths){
-        return false;
-    }
-
-    /**
-     * 获取对应ID的故事目录路径
-     * @param storyId Story模板id
-     * @return 故事目录路径
-     */
-    public String getStoryPath(int storyId){
-        return null;
-    }
-
-    /**
-     * 获取对应ID的模版目录路径
-     * @param stageId 模板id
-     * @return 模板目录路径
-     */
-    public String getStagePath(int stageId){
-        return null;
-    }
-
-    /**
-     * 读取HTML文件
-     * @param path 文件路径
-     * @return 文件内容
-     */
-    public String read(String path){
-        return null;
-    }
-
-    /**
-     *用户新增资源文件的硬盘路径，被替换的文件路径
-     * @param sourcePath 源路径
-     * @param destPath 被替换路径
-     */
-    public boolean replaceFile(String sourcePath,String destPath){
-        return false;
-    }
-
-    /**
-     * JS调用接口，发起下载
-     * @param type
-     * @param id
-     * @return
-     */
-    public Object start(int type, int id){
-        return null;
     }
 
     @Override
