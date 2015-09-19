1b88ba689756d67d6229fcddda258a95499251eb
liushiping <6423669@qq.com>
2015-09-20 02:17:22 +0800

    修改封面

--- app/src/main/java/com/wisape/android/cordova/StoryTemplatePlugin.java
+++ app/src/main/java/com/wisape/android/cordova/StoryTemplatePlugin.java
@@ -21,7 +21,6 @@ import com.wisape.android.logic.StoryLogic;
 import com.wisape.android.model.StoryFontInfo;
 import com.wisape.android.model.StoryTemplateInfo;
 import com.wisape.android.network.Requester;
-import com.wisape.android.util.EnvironmentUtils;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.cordova.CallbackContext;
@@ -34,13 +33,15 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -302,12 +303,17 @@ public class StoryTemplatePlugin extends AbsPlugin{
                 if (!myStory.exists()){
                     myStory.mkdirs();
                 }
-                if(!saveStory(myStory,html,paths)){
+                if(!saveStory(myStory, html, paths)){
                     callbackContext.error(-1);
                     return null;
                 }
+                if ("".equals(story.storyThumbUri)
+                        && !new File(story.storyThumbUri).exists()){
+                    copyAssetsFile("www/public/img/photo_cover.png",
+                            new File(story.storyLocal, "thumb.png").getAbsolutePath());
+                }
                 ApiStory.AttrStoryInfo storyAttr = new ApiStory.AttrStoryInfo();
-                Uri thumb = Uri.fromFile(new File(story.storyLocal, "thumb.jpeg"));
+                Uri thumb = Uri.fromFile(new File(story.storyLocal, "thumb.png"));
                 storyAttr.attrStoryThumb = thumb;
                 storyAttr.storyStatus = ApiStory.AttrStoryInfo.STORY_STATUS_RELEASE;
                 storyAttr.story = Uri.fromFile(new File(story.storyLocal));
@@ -388,6 +394,41 @@ public class StoryTemplatePlugin extends AbsPlugin{
             }
         }
         return true;
+    }
+
+    private void copyAssetsFile(String src, String dest) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(dest);
+            in = cordova.getActivity().getAssets().open(src);
+            byte[] buffer = new byte[1024];
+            int length = in.read(buffer);
+            while (length > 0) {
+                out.write(buffer, 0, length);
+                length = in.read(buffer);
+            }
+            out.flush();
+            in.close();
+            out.close();
+        }catch (IOException e){
+
+        }finally {
+            if (in != null){
+                try {
+                    in.close();
+                }catch (IOException  e){
+
+                }
+            }
+            if (out != null){
+                try {
+                    out.close();
+                }catch (IOException  e){
+
+                }
+            }
+        }
     }
 
     private String readFile(String path){
